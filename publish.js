4ca14307c2c37112153f68820af4c9bc89d2c934
mtmg32 <manuelmaiagoncalves@gmail.com>
2016-08-10 12:55:13 +0100

    latest update 1470830113069

--- publish.js
+++ publish.js
@@ -14,12 +14,15 @@ if (!Date.now) {
 }
 
 //var name = "Your Github Name";
+var name = "mtmg32";
 //var email = "Your Github email";
-
+var email = "manuelmaiagoncalves@gmail.com";
 //console.log("Configuring git");
+console.log("Configuring git");
 
-//exec("git config --global user.name " + name, printer);
-//exec("git config --global user.email " + email, printer);
+exec("git config --global user.name " + name, printer);
+
+exec("git config --global user.email " + email, printer);
 
 console.log("Commiting code locally");
 exec("git add .", printer);
